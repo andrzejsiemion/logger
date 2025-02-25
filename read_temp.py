3e8492545b07343450fba6e2f690b379444e283d
Andrzej Siemion <36746797+andrzejsiemion@users.noreply.github.com>
2025-02-26 00:16:53 +0100

    Cleanup code

--- read_temp.py
+++ read_temp.py
@@ -28,7 +28,6 @@ logger.info("Starting DHT logger...")  # Debugging message
 LOG_INTERVAL = int(os.getenv("LOG_INTERVAL", 60)) # Default 60 seconds if not set
 DHT_PIN = os.getenv("DHT_PIN", "D4")  # Default to D4 if not set
 DHT_RETRIES = int(os.getenv("DHT_RETRIES", 3)) # Default 3 retries if not set
-
 DHT_SENSOR_NAME = os.getenv("DHT_SENSOR_NAME", f"sensor_{DHT_PIN}")
 
 logger.info(f"Using DHT_PIN: {DHT_PIN}")  # Debugging message
